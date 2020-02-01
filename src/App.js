05cfb239f08d0c1dbd0a4800024f672c753ea1be
Alexsander <alexsander.zuniga@readinessit.com>
2020-01-31 21:26:02 -0300

    Create addTweet button and modal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Container, Snackbar} from '@material-ui/core'
 import Header from './components/Header';
+import SendTweet from './components/SendTweet';
+import ModalContainer from './components/ModalContainer';
 
 function App() {
   return (
     <Container className="tweets-simulator" maxWidth={false}>
       <Header/>
+      <SendTweet/>
     </Container>
   );
 }
