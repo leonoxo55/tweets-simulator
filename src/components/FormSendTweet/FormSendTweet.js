a686e57d7d284d895a71cb052917e35a456dc329
leonoxo <alex71.sander71@gmail.com>
2020-01-31 23:58:30 -0300

    add form to send tweet in modal component

---
+++ src/components/FormSendTweet/FormSendTweet.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { FormControl, FormGroup, TextField, Button } from "@material-ui/core";
+
+import "./FormSendTweet.scss";
+
+export default function FormSendTweet(props) {
+  const {} = props;
+  return (
+    <div className="form-send-tweet">
+      <h2 className="form-send-tweet__title">Enviar Tweet</h2>
+      <form className="form-send-tweet__form">
+        <FormControl>
+          <FormGroup>
+            <TextField
+              className="form-send-tweet__form-name"
+              type="text"
+              name="name"
+              placeholder="Nombre del usuario"
+              margin="normal"
+            />
+            <TextField
+              className="form-send-tweet__form-textarea"
+              name="tweet"
+              multiline
+              rows="6"
+              placeholder="Escribe tu Tweet"
+              margin="normal"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Button type="submit">Enviar Tweet</Button>
+          </FormGroup>
+        </FormControl>
+      </form>
+    </div>
+  );
+}
